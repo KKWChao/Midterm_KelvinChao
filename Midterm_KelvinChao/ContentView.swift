5ab7256b4fe97498953f9405e31062a7dd699795
Kelvin <kelvinchao@protonmail.com>
2025-10-18 15:47:42 -0700

    Initial Commit

---
+++ Midterm_KelvinChao/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Midterm_KelvinChao
+//
+//  Created by Kelvin Chao on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Midterm_KelvinChao/Midterm_KelvinChaoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Midterm_KelvinChaoApp.swift
+//  Midterm_KelvinChao
+//
+//  Created by Kelvin Chao on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct Midterm_KelvinChaoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
